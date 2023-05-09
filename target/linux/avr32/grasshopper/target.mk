46398562da8ad68a6b5ab115dac6c00eed170042
Harry Zusch <HarryZusch@alice.de>
2023-05-09 11:19:54 +0200

    initial commit of r20651

---
+++ target/linux/avr32/grasshopper/target.mk
@@ -0,0 +1,7 @@
+BOARDNAME:=Grasshopper
+FEATURES:=jffs2
+
+define Target/Description
+	Build firmware images for Grasshopper board 
+endef
+
---
+++ target/linux/avr32/ngw100/target.mk
@@ -0,0 +1,8 @@
+BOARDNAME:=NGW100
+FEATURES:=squashfs
+
+define Target/Description
+  Build firmware images for ATNGW100 board
+endef
+
+
